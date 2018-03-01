a3f713753298bb19969c56f4030dca3ebcc1a06b
Amila Fernando <amila_fernandp92@gmail.com>
2018-03-01 11:26:06 +0100

    modifiche

--- JAVASCRIPT/jsfunction.js
+++ JAVASCRIPT/jsfunction.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     $.getJSON("https://randomuser.me/api/?results=1&gender=female", function(data) {
         globalvar = data;
         $('#icon').attr('src', data.results[0].picture.large);
+        $('#loader').css("display", "none");
     });
 });
 
@@ -40,6 +41,9 @@ function addImage (app) {
         break;
         case 4 :
             img.src = "../IMG/about_us.png";
+        break;
+        case 5 :
+            img.src = "../IMG/privacy.png";
         break;
     }
     li.appendChild (img);
@@ -103,4 +107,58 @@ function removeText () {
     var h1app = document.getElementById ("bigtext");
     papp.innerText = "";
     h1app.innerText = "";
+}
+
+function createFooter () {
+    var divsubcontainer = document.createElement ("div");
+    divsubcontainer.className = "subcontainer";
+        var divcontup = document.createElement ("div");
+        divcontup.id = "contup";
+            var divtitle = document.createElement ("div");
+            divtitle.id = "title";
+                var h1 = document.createElement ("h1");
+                    h1.innerText = "RANDOM USER GENERATOR";
+                divtitle.appendChild (h1);
+            divcontup.appendChild (divtitle);
+        divsubcontainer.appendChild (divcontup);
+        var divcontdown = document.createElement ("div");
+        divcontdown.id = "contdown";
+            var divdeveloper = document.createElement ("div");
+            divdeveloper.id = "developer";
+                var h5 = document.createElement ("h5");
+                    h5.innerText = "Web Developer";
+                divdeveloper.appendChild (h5);
+                var p = document.createElement ("p");
+                    p.innerText = "Developed By Amila Fernando";
+                divdeveloper.appendChild (p);
+            divcontdown.appendChild (divdeveloper);
+            var divcopyright = document.createElement ("div");
+            divcopyright.id = "copyright";
+                var h5 = document.createElement ("h5");
+                    h5.innerText = "Copyright Notice";
+                divcopyright.appendChild (h5);
+                p1 = "<p>All randomly generated photos were hand picked from the authorized section of <a href=\"http://uifaces.com/\">UI Faces</a>. Please visit <a href=\"https://web.archive.org/web/20160811185628/http://uifaces.com/faq\">UI Faces</a> FAQ for more information regarding how you can use these faces.</p>";
+                divcopyright.insertAdjacentHTML('beforeEnd', p1);
+                p2 = "<p>API Generation is from <a href=\"https://randomuser.me/\">Random users</a>.</p>";
+                divcopyright.insertAdjacentHTML('beforeEnd', p2);
+            divcontdown.appendChild (divcopyright);
+        divsubcontainer.appendChild (divcontdown);
+    var footer = document.getElementById ("footer");
+    footer.appendChild (divsubcontainer);
+}
+
+function show () {
+    document.getElementById ("pwd").setAttribute ("type", "text");  
+}
+
+function hide () {
+    document.getElementById ("pwd").setAttribute ("type", "password");   
+}
+
+function showHide () {
+    if (document.getElementById ("pwd").getAttribute ("type") === "password") {
+        show ();
+    } else {
+        hide ();
+    }
 }
